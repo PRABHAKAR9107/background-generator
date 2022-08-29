358d29ef995fa088464e479b477bf2551a2dfc3f
PRABHAKAR9107 <02panjiyara@gmail.com>
2022-08-29 17:34:04 +0530

    Add css styles for h3 element

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-var css = document.querySelector("h3");
+var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
-	css.textContent = "colorcode: " + body.style.background + ";";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  h3.innerText = "colorcode: " + body.style.background + ";";
 }
 
+color1.addEventListener("input", setGradient);
 
-color1.addEventListener("input",setGradient);
+color2.addEventListener("input", setGradient);
 
-color2.addEventListener("input",setGradient);
+h3.style.border = "1px solid  black";
 
 console.log(css);
 
